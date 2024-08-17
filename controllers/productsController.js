6375261e73396b51a9b3baa7f962038bf9dee564
Sharidom Md Noushan <sm.noushan@gmail.com>
2024-08-17 13:32:01 +0600

    update: db.get all products --added get producs by search value

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,19 @@
 const getProductsCollection = require("../models/productsModel");
 
+// Function to generate a random date
+function generateRandomDate() {
+  const start = new Date(2024, 0, 1); // Starting date
+  const end = new Date(); // Current date
+  const randomTime =
+    start.getTime() + Math.random() * (end.getTime() - start.getTime());
+  return new Date(randomTime).toISOString(); // Convert to ISO string
+}
+
 // Retrieve all tasks
 const getAllProducts = async (req, res) => {
+  const searchTerm = req.query?.search || "";
+  // Create a case-insensitive regex for partial matching
+  const regex = new RegExp(searchTerm, "i"); // 'i' for case-insensitive
   const productsCollection = await getProductsCollection();
   const projection = {
     title: 1,
@@ -13,7 +25,9 @@ const getAllProducts = async (req, res) => {
     meta: { createdAt: 1 },
     thumbnail: 1,
   };
-  const products = await productsCollection.find({}, { projection }).toArray();
+  const products = await productsCollection
+    .find({ title: { $regex: regex } }, { projection })
+    .toArray();
   res.json(products);
 };
 
